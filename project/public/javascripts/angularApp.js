59c379267a5e8b15eca437af03edca76c8ce7e19
dustin-kavanaugh <dustinchay@gmail.com>
2017-10-19 13:12:51 -0700

    added bare bones RecipientCtrl

--- project/public/javascripts/angularApp.js
+++ project/public/javascripts/angularApp.js
@@ -251,3 +251,25 @@ app.controller('PostsCtrl', [
         };
 }]);
 
+app.controller('RecipientCtrl', [
+    '$scope',
+    'recipients',
+    'recipient',
+    'auth',
+    function($scope, recipients, recipient, auth){
+        $scope.recipient = recipient;
+        $scope.isLoggedIn = auth.isLoggedIn;
+        // $scope.addComment = function(){
+        //     if($scope.body === '') { return; }
+        //     posts.addComment(post._id, {
+        //       body: $scope.body,
+        //       author: 'user',
+        //     }).success(function(comment) {
+        //       $scope.post.comments.push(comment);
+        //     });
+        //     $scope.body = '';
+        //   };
+        // $scope.incrementUpvotes = function(comment){
+        //   posts.upvoteComment(post, comment);
+        // };
+}]);
